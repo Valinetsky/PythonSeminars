ea0e16ded012e3ea1ea55d88d4dc841635560308
Терёшин Роман <rtereshin@mac-55450192.users.rbc.ru>
2023-02-26 21:22:16 +0300

    Seminar04

--- Seminar03/HomeWorkTask20.py
+++ Seminar03/HomeWorkTask20.py
@@ -31,33 +31,65 @@ def langCheck(word, langEn, langRu):
 
 # Функция, считающая суммарный вес слова
 def countScore(word):
-    caseLetter01 = {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т'}
-    caseLetter02 = {'D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'}
-    caseLetter03 = {'B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'}
-    caseLetter04 = {'F', 'H', 'V', 'W', 'Y', 'Й', 'Ы'}
-    caseLetter05 = {'K', 'Ж', 'З', 'Х', 'Ц', 'Ч'}
-    caseLetter08 = {'J', 'X', 'Ш', 'Э', 'Ю'}
-    caseLetter10 = {'Q', 'Z', 'Ф', 'Щ', 'Ъ'}
+
+    # caseLetter01 = {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т'}
+    # caseLetter02 = {'D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'}
+    # caseLetter03 = {'B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'}
+    # caseLetter04 = {'F', 'H', 'V', 'W', 'Y', 'Й', 'Ы'}
+    # caseLetter05 = {'K', 'Ж', 'З', 'Х', 'Ц', 'Ч'}
+    # caseLetter08 = {'J', 'X', 'Ш', 'Э', 'Ю'}
+    # caseLetter10 = {'Q', 'Z', 'Ф', 'Щ', 'Ъ'}
+
+
+    price = {'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'S': 1, 
+             'T': 1, 'R': 1, 'А': 1, 'В': 1, 'Е': 1, 'И': 1, 'Н': 1, 'О': 1, 
+             'Р': 1, 'С': 1, 'Т': 1,
+             'А': 1, 'В': 1, 'Е': 1, 'И': 1, 'Н': 1, 'О': 1, 'Р': 1, 'С': 1, 
+             'Т': 1,
+
+             'D': 2, 'G': 2, 
+             'Д': 2, 'К': 2, 'Л': 2, 'М': 2, 'П': 2, 'У': 2,
+
+             'B': 3, 'C': 3, 'M': 3, 'P': 3, 
+             'Б': 3, 'Г': 3, 'Ё': 3, 'Ь': 3, 'Я': 3,
+
+             'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4, 
+             'Й': 4, 'Ы': 4,
+
+             'K': 5, 
+             'Ж': 5, 'З': 5, 'Х': 5, 'Ц': 5, 'Ч': 5,
+
+             'J': 8, 'X': 8, 
+             'Ш': 8, 'Э': 8, 'Ю': 8,
+
+             'Q': 10, 'Z': 10, 
+             'Ф': 10, 'Щ': 10, 'Ъ': 10
+    }
     
     counter = 0
 
     localWord = word.upper()
 
+    # for letter in localWord:
+    #     if letter in caseLetter01:
+    #         counter += 1
+    #     if letter in caseLetter02:
+    #         counter += 2
+    #     if letter in caseLetter03:
+    #         counter += 3
+    #     if letter in caseLetter04:
+    #         counter += 4
+    #     if letter in caseLetter05:
+    #         counter += 5
+    #     if letter in caseLetter08:
+    #         counter += 8
+    #     if letter in caseLetter10:
+    #         counter += 10
+    
+    # return counter
+
     for letter in localWord:
-        if letter in caseLetter01:
-            counter += 1
-        if letter in caseLetter02:
-            counter += 2
-        if letter in caseLetter03:
-            counter += 3
-        if letter in caseLetter04:
-            counter += 4
-        if letter in caseLetter05:
-            counter += 5
-        if letter in caseLetter08:
-            counter += 8
-        if letter in caseLetter10:
-            counter += 10
+        counter += price[letter]
     
     return counter
 
---
+++ Seminar04/Task27.py
@@ -0,0 +1,23 @@
+# Задача No27. 
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
+# Output: 13
+
+someText = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
+
+apperText = someText.upper()
+
+wordsArr = apperText.split()
+
+resultArr = set(wordsArr)
+
+print(len(resultArr))
+print(resultArr)
+
